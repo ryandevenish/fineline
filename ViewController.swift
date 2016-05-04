7a6169974b6b99993bcf2a77e0696115fc096b1b
Ryan Devenish <ryan.devenish@opower.com>
2016-05-03 17:51:14 -0700

    updated the storyboard

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fineline
+//
+//  Created by Ryan Devenish on 4/30/16.
+//  Copyright © 2016 Ryan Devenish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
